6085f3ff153ca709097dc2b50c7c90204ac94e83
karasu . <timothy.2k21@gmail.com>
2024-07-04 17:50:01 +0300

    map Markers fix

--- src/App.js
+++ src/App.js
@@ -24,24 +24,26 @@ function App() {
 
   // Custom icons
   const customIcon = new Icon ({
-    iconUrl: "",
+    iconUrl: "https://cdn-icons-png.flaticon.com/128/798/798008.png",
     iconSize: [38, 38]
   }
 
-  )
+  );
 
   return (
     <div>
   <MapContainer center={[-.1318, 36.81]} zoom={7}>
     <TileLayer 
-    attribution ='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution ='&copy; <a href="https://www.openstreetmap.org/copyright">Ujamaa Africa</a> '
     url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
     />
 
     {
       markers.map(marker =>(
-      <Marker position={marker.geocode}>
-
+      <Marker position={marker.geocode}
+        icon={customIcon}
+      >
+          
       </Marker>
       ))
   
