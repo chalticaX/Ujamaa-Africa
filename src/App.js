9068ecaae3e9f8dd022faea0eed58c6e2079cdbd
karasu . <timothy.2k21@gmail.com>
2024-07-10 16:21:18 +0300

    added Migori cluster

--- src/App.js
+++ src/App.js
@@ -1,60 +1,58 @@
-import './App.css';
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import "leaflet/dist/leaflet.css";
+import React from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 import { Icon } from 'leaflet';
+import polygonJSON from './polygon.js';
+import { L } from "leaflet";
 
 function App() {
-  // Layer
-  
-
+  // Icons
+  const customIcon = new Icon({
+    iconUrl: 'https://cdn-icons-png.flaticon.com/128/798/798008.png',
+    iconSize: [38, 38],
+  });
 
   // Co-ordinates
+const markers = [
+  {
+    geocode: [-1.253, 36.8844],
+    popUp: 'Ujamaa Office',
+  },
+  {
+    geocode: [-1.2866, 36.8174],
+    popUp: 'Nairobi',
+  },
+  {
+    geocode: [-1.21555, 36.83514],
+    popUp: 'Kiambu road',
+  },
+];
 
-  const markers = [
-    { 
-      geocode: [-1.253, 36.8844],
-      popUp: "Ujamaa Oficce"
-    },
-    {
-      geocode: [-1.2866, 36.8174],
-      popUp: "Nairobi"
-    },
-    {
-      geocode: [-1.21555, 36.83514],
-      popUp: "Kiambu road"
-    },
-  ];
 
-  // icons
-  const customIcon = new Icon ({
-    iconUrl: "https://cdn-icons-png.flaticon.com/128/798/798008.png",
-    iconSize: [38, 38]
-  }
-
-  );
 
   return (
     <div>
-  <MapContainer center={[-.1318, 36.81]} zoom={7}>
-    <TileLayer 
-    attribution ='&copy; <a href="https://www.openstreetmap.org/copyright">Ujamaa Africa</a> '
-    url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-    />
+      <MapContainer center={[-1.1318, 36.81]} zoom={7} style={{ height: '100vh' }}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">Ujamaa Africa</a> '
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
 
-    {
-      markers.map(marker =>(
-      <Marker position={marker.geocode}
-        icon={customIcon}>
-        <Popup>
-            {marker.popUp}
-          </Popup>  
-      </Marker>
-      ))
-  
-    }
+        {/* markers */}
+        {markers.map((marker, index) => (
+          <Marker key={index} position={marker.geocode} icon={customIcon}>
 
-  </MapContainer>
-  </div>
+            <Popup>{marker.popUp}</Popup>
+          </Marker>
+        ))}
+        <GeoJSON
+          data={polygonJSON}
+          style={{ fillColor: 'blue', color: 'blue', weight: 2, fillOpacity: 0.5 }}
+        
+        />
+      
+      </MapContainer>
+    </div>
   );
 }
 
