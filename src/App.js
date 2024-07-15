e8b3c1f2b29a70ed4746d7c4369c85f499ba07b8
karasu . <timothy.2k21@gmail.com>
2024-07-15 12:41:09 +0300

    adding details tab

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { MapContainer, TileLayer, Popup, Marker, GeoJSON, LayersControl } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, GeoJSON, LayersControl } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import { Icon } from 'leaflet';
 import polygonJSON from './polygon.js';
@@ -16,17 +16,17 @@ import 'react-leaflet-markercluster/dist/styles.min.css';
 import Legend from './legend.js';
 
 function App() {
+  const [clickedFeature, setClickedFeature] = useState(null);
 
-  // GeoJSON Array
-  const geoJsonDataArray = [kenyaJSON, polygonJSON, westernJSON ,kajiadoJSON, kiambuJSON, machakosJSON, nairobiJSON, nakuruJSON];
+  const geoJsonDataArray = [kenyaJSON, polygonJSON, westernJSON, kajiadoJSON, kiambuJSON, machakosJSON, nairobiJSON, nakuruJSON];
 
   const handleFeatureClick = (event) => {
     const layer = event.target;
-    console.log('Feature clicked:', layer.feature);
+    setClickedFeature(layer.feature.properties);
   };
 
   const handleMouseOver = (event) => {
-    const layer = event.target
+    const layer = event.target;
     layer.setStyle({
       weight: 5,
       color: '#666',
@@ -37,7 +37,7 @@ function App() {
   };
 
   const handleMouseOut = (event) => {
-    const layer = event.target
+    const layer = event.target;
     layer.setStyle({
       weight: 4,
       color: 'black',
@@ -47,10 +47,9 @@ function App() {
     layer.closePopup();
   };
 
-
   const onEachFeature = (feature, layer) => {
-     const popupContent = `${feature.properties.name}`;
-     layer.bindPopup(popupContent);
+    const popupContent = `${feature.properties.name}`;
+    layer.bindPopup(popupContent);
 
     layer.on({
       click: handleFeatureClick,
@@ -59,15 +58,12 @@ function App() {
     });
   };
 
-
-  // Icons
   const customIcon = new Icon({
     iconUrl: 'https://img.icons8.com/?size=100&id=43731&format=png&color=000000',
     iconSize: [38, 38],
     popupAnchor: [0, -38],
   });
 
-  // Coordinates
   const markers = [
     {
       geocode: [-1.253, 36.8844],
@@ -83,72 +79,79 @@ function App() {
     },
   ];
 
-
   return (
-    <div>
-      <MapContainer center={[-1.1318, 36.81]} zoom={6} style={{ height: '100vh' }}>
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">Ujamaa Africa</a>'
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-
-        {/* MARKERS */}
-        <MarkerClusterGroup>
-          {markers.map((marker, index) => (
-            <Marker key={index} position={marker.geocode} icon={customIcon} >
-            
-            </Marker>
-          ))}
-        </MarkerClusterGroup> 
-
-        {/* GEOJSON */}
-        {geoJsonDataArray.map((data, index) => (
-          <GeoJSON
-            key={index}
-            data={data}
-            style={{ color: 'black', weight: 2, fillOpacity: 0.0, dashArray: 3 }}
-            onEachFeature={onEachFeature}
+    <div style={{ display: 'flex' }}>
+      <div style={{ flex: 1, height: '100vh' }}>
+        <MapContainer center={[-1.1318, 36.81]} zoom={6} style={{ height: '100vh' }}>
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">Ujamaa Africa</a>'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-        ))}
 
-        {/* LAYER CONTROLS */}
-        <LayersControl position="topright">
-          <LayersControl.BaseLayer checked name="Default Street Map">
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-          </LayersControl.BaseLayer>
+          <MarkerClusterGroup>
+            {markers.map((marker, index) => (
+              <Marker key={index} position={marker.geocode} icon={customIcon} />
+            ))}
+          </MarkerClusterGroup>
 
-          <LayersControl.BaseLayer name="Dark palette">
-            <TileLayer
-              url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png?apikey=4bc5ae1a-e40d-4030-b17d-8079a33b8ba3"
-              attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              minZoom={0}
-              maxZoom={20}
-              ext="png"
+          {geoJsonDataArray.map((data, index) => (
+            <GeoJSON
+              key={index}
+              data={data}
+              style={{ color: 'black', weight: 2, fillOpacity: 0.0, dashArray: 3 }}
+              onEachFeature={onEachFeature}
             />
-          </LayersControl.BaseLayer>
+          ))}
 
-          <LayersControl.BaseLayer name="Stadia Stamen Watercolor">
-            <TileLayer
-              url='https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg?apikey=4bc5ae1a-e40d-4030-b17d-8079a33b8ba3'
-              attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              minZoom={0}
-              maxZoom={16}
-              ext="jpg"
-            />
-          </LayersControl.BaseLayer>
+          <LayersControl position="topright">
+            <LayersControl.BaseLayer checked name="Default Street Map">
+              <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            </LayersControl.BaseLayer>
 
-          <LayersControl.BaseLayer name="Satellite View">
-            <TileLayer
-              url='https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.jpg?apikey=4bc5ae1a-e40d-4030-b17d-8079a33b8ba3'
-              attribution='&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              minZoom={0}
-              maxZoom={20}
-              ext="jpg"
-            />
-          </LayersControl.BaseLayer>
-        </LayersControl>
-        <Legend />
-      </MapContainer>
+            <LayersControl.BaseLayer name="Dark palette">
+              <TileLayer
+                url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png?apikey=4bc5ae1a-e40d-4030-b17d-8079a33b8ba3"
+                attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                minZoom={0}
+                maxZoom={20}
+                ext="png"
+              />
+            </LayersControl.BaseLayer>
+
+            <LayersControl.BaseLayer name="Stadia Stamen Watercolor">
+              <TileLayer
+                url='https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg?apikey=4bc5ae1a-e40d-4030-b17d-8079a33b8ba3'
+                attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://www.stamen.com/" target="_blank">Stamen Design</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                minZoom={0}
+                maxZoom={16}
+                ext="jpg"
+              />
+            </LayersControl.BaseLayer>
+
+            <LayersControl.BaseLayer name="Satellite View">
+              <TileLayer
+                url='https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.jpg?apikey=4bc5ae1a-e40d-4030-b17d-8079a33b8ba3'
+                attribution='&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                minZoom={0}
+                maxZoom={20}
+                ext="jpg"
+              />
+            </LayersControl.BaseLayer>
+          </LayersControl>
+          <Legend />
+        </MapContainer>
+      </div>
+      <div style={{ flex: 1, padding: '10px', borderLeft: '1px solid #ccc', overflowY: 'auto' }}>
+        {clickedFeature ? (
+          <div>
+            <h2>Feature Information</h2>
+            <p>Name: {clickedFeature.name}</p>
+            {/* Add more properties as needed */}
+          </div>
+        ) : (
+          <p>Click on a feature to see its details</p>
+        )}
+      </div>
     </div>
   );
 }
