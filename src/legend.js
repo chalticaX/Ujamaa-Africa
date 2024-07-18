adb9666fda30d3c12954125afba894b7729ba218
karasu . <timothy.2k21@gmail.com>
2024-07-18 21:16:03 +0300

    styling 1.3

--- src/legend.js
+++ src/legend.js
@@ -4,7 +4,8 @@ const Legend = () => {
   return (
     <div className="legend">
       <h4>Map Legend</h4>
-      <div><span className="legend-icon" style={{ backgroundColor: '#d76c27' }}></span> YEAR ONE COUNTIES</div>
+      <div><span className="legend-icon" style={{ backgroundColor: '#d76c27' }}></span> YEAR 2023 COUNTIES</div>
+      <div><span className="legend-icon" style={{ backgroundColor: '#640522' }}></span> YEAR 2024 COUNTIES</div>
       <div><span className="legend-icon" style={{ backgroundColor: '#999b37' }}></span> TARGET LEARNERS</div>
       <div><span className="legend-icon" style={{ backgroundColor: 'blue' }}></span> LEARNERS TRAINED</div>
     </div>
